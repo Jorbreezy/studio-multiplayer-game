b9d39a1d725b4d0d89eb8f04c66273ffb438a81f
Jordan Kelly <jkelly2@aoiths.org>
2018-04-30 22:26:42 +0000

    hello

--- src/TwentyOneQuestions.js
+++ src/TwentyOneQuestions.js
@@ -4,30 +4,70 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import TextField from 'material-ui/TextField';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
-
-const gameMessage = [
-    "Name 5 States",
-    "What is the 5th president of the United States?",
-    "What are the 5 Boroughs?",
-    "How many State in the USA",
-    "What year was America found",
-    "What is the name of a Squirrels home ?",
-    "Blue, Pilot and Minke are all species of what ?",
-    "From what animal do we get Cashmere ?",
-    "How many arms does a Starfish usually have ?",
-    "How many eyes does a bee have ?",
-    "How many legs does a lobster use for walking ?",
-    "How many planets in our Solar System have only one moon ?",
-    "How many legs / tentacles does a squid have ?",
-    "What is a Baby Oyster called ?",
-    "What is a female alligator called?",
-    "A scaup is what type of bird?",
-    "How many toes does a cat have on each front paw?",
-    "What is the head of a jellyfish called?",
-    "An oribi is what type of animal?",
-    "Dry ice is a frozen form of which gas?"
-];
+const gameMessage = [[
+        "Which ocean lies on the east coast of the United States?",
+        "Atlantic Ocean"
+    ],[
+        "What is the 5th president of the United States?",
+        "Donald Trump"
+    ],[
+        "What are the 5 Boroughs?",
+        "Brooklyn, Queens, Staten Island, Manhatten"
+    ],[
+        "How many State in the USA?",
+        "50"
+    ],[
+        "What year was America found?",
+        "1600"
+    ],[
+        "What is the worlds largest mountain?",
+        "Mount Everest"
+    ],[
+        "How many great lakes are there?",
+        "5"
+    ],[
+        "What is the longest River in the US?",
+        "Missouri River"
+    ],[
+        "The biggest desert in the world is...",
+        "Sahara"
+    ],[
+        "How many Kindoms is the United Kindom comprised of?",
+        "4"
+    ],[
+        "Dry Ice is a Frozen form of which gas?",
+        "Carbon Dioxide"
+    ],[
+        "What is the longest river in the world?",
+        "Nile River"
+    ],[
+        "What is the largest body of water?",
+        "Pacific Ocean"
+    ],[
+        "What is the most popular country?",
+        "China"
+    ],[
+        "What is the capital of IceLand?",
+        "Reykjavik"
+    ],[
+        "What continent is the largest in terms of land?",
+        "Asia"
+    ],[
+        "What is the largest mountain range in the world?",
+        "Andes"
+    ],[
+        "What is the smallest country in the world?",
+        "Vatican City"
+    ],[
+        "What is the river that runs through Paris, France is called?",
+        "The Seine"
+    ],[
+        "What is the most common speaking language in the world?",
+        "English"
+    ]];
 
 export default class TwentyOneQuestions extends Component {
     constructor(props) {
@@ -36,7 +76,8 @@ export default class TwentyOneQuestions extends Component {
             choice: "",
             opponent: "",
             message: "",
-            hidden: true
+            hidden: true,
+            open: false
         };
     }
 
@@ -53,16 +94,54 @@ export default class TwentyOneQuestions extends Component {
             hidden: !this.state.hidden
         })
     }
+    
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+     };
+
+    
+    check(checks){
+        if(checks === gameMessage[1]){
+            alert("Correct");
+        } else {
+            alert("Incorrect");
+        }
+    }
 
     render() {
         const list = gameMessage.map((item) => {
-            return <li key={item} onClick={() => console.log(item)}> {item} </li>
+            return <li className = "question" key={item} onClick={() => this.handleOpen()}> {item[0]} </li>
         });
+
+        const actions = [
+              <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+              />,
+              <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+              />,
+        ];
+
+
+        const inpt = () => (
+            <Dialog title="Answer Question" actions={actions} modal={false} open={this.state.open} onRequestClose={this.handleClose}>
+                <TextField id="text-field-default" placeholder="Answer"/>
+            </Dialog>
+        );
 
         const txt = () => (
             <div>
-                <TextField id="text-field-default" defaultValue="Question"/><br />
-                <TextField id="text-field-default" defaultValue="Answer"/><br />
+                <TextField id="text-field-default" placeholder="Question"/><br />
+                <TextField id="text-field-default" placeholder="Answer"/><br />
              </div>
         );
 
@@ -76,9 +155,9 @@ export default class TwentyOneQuestions extends Component {
       >
         <Tab label="Questions" value="a">
           <div>
-            <ul>
+            <ol>
                 {list}
-            </ul>
+            </ol>
           </div>
         </Tab>
         <Tab label="Input your own Question" value="b">
@@ -89,6 +168,8 @@ export default class TwentyOneQuestions extends Component {
           </div>
         </Tab>
       </Tabs>
+      
+      {inpt()}
             </div>
           </div>);
     }
